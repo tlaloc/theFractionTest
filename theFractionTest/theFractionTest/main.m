2050577de92abc2bc0c7eb7db093258ca6d23651
Stephen Franklin <stephenLfranklin@gmail.com>
2012-05-16 07:55:35 -0500

    test commit

---
+++ theFractionTest/theFractionTest/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  theFractionTest
+//
+//  Created by Not Sure on 5/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+		// TESTING
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
