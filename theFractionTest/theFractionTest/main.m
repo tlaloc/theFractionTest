9ee6b174fe020bf7f0d84b69b238110dfcdaa385
Stephen Franklin <stephenLfranklin@gmail.com>
2012-05-16 08:12:25 -0500

    fraction files added

--- theFractionTest/theFractionTest/main.m
+++ theFractionTest/theFractionTest/main.m
@@ -1,24 +1,87 @@
 //
 //  main.m
-//  theFractionTest
+//  FractionTest
 //
-//  Created by Not Sure on 5/16/12.
+//  Created by Not Sure on 5/12/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+/*
+ #import "Fraction.h"
+ int main (int argc, char * argv[]) {
+ @autoreleasepool {
+ Fraction *myFraction = [[Fraction alloc] init];
+ 
+ // set fraction to 1/3
+ 
+ [myFraction setNumerator: 1]; [myFraction setDenominator: 3];
+ 
+ // display the fraction
+ 
+ NSLog (@"The value of myFraction is:");
+ [myFraction print]; 
+ }
+ return 0; 
+ }
+ 
+ 
+ #import "Fraction.h"
+ 
+ int main (int argc, char * argv[]) {
+ 
+ @autoreleasepool {
+ Fraction *aFraction = [[Fraction alloc] init];
+ 
+ [aFraction setTo: 100 over: 200]; 
+ [aFraction print];
+ 
+ [aFraction setTo: 1 over: 3];
+ [aFraction print]; 
+ }
+ return 0; 
+ }
+ */
 
-int main(int argc, const char * argv[])
-{
+#import "Fraction.h"
 
+int main (int argc, char * argv[]) {
+	
 	@autoreleasepool {
-	    
-	    // insert code here...
-		// TESTING 
-		// AND TESTING BACK
-	    NSLog(@"Hello, World!");
-	    
+		
+		Fraction *aFraction = [[Fraction alloc] init]; 
+		Fraction *bFraction = [[Fraction alloc] init];
+		
+		Fraction *cFraction;
+		
+		// Set two fractions to 1/4 and 1/2 and add them together
+		
+		[aFraction setTo: -5 over: 2]; 
+		[bFraction setTo: 2 over: 7];
+		
+		// Print the results
+		
+		[aFraction print: NO]; 
+		NSLog (@"+"); 
+		[bFraction print: NO]; 
+		NSLog (@"=");
+		
+		cFraction = [aFraction add: bFraction];
+		[cFraction print: NO]; 
+		
+		cFraction = [aFraction subtract: bFraction];
+		[cFraction print: NO];
+		
+		cFraction = [aFraction multiply: bFraction];
+		[cFraction print: NO];
+		
+		cFraction = [aFraction divide: bFraction];
+		[cFraction print: NO];
+		
+		[aFraction print: YES]; 
+		NSLog (@"  reduced!"); 
+		[bFraction print: YES]; 
+		NSLog (@" reduced!");
+		
 	}
-    return 0;
+	return 0; 
 }
-
